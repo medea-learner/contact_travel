fc8db16a5f8acc38147a2ff6b067b2c3ddeacf1a
Mohamed <mohamed.ea.der@gmail.com>
2025-04-14 14:56:51 +0100

    Module first setup

---
+++ __manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'Contact Travel',
+    'version': '1.0',
+    'summary': 'Manage user travels and reward levels',
+    'category': 'Tools',
+    'installable': True,
+    'application': True,
+}

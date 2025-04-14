ce86fe254e13570bb584d56fc7c067df233d3188
Mohamed <mohamed.ea.der@gmail.com>
2025-04-14 15:02:52 +0100

    Add Voyage model

---
+++ models/voyage.py
@@ -0,0 +1,14 @@
+from odoo import models, fields
+
+class Voyage(models.Model):
+    _name = 'contact_travel.voyage'
+    _description = 'Voyage'
+
+    name = fields.Char(string='Nom du voyage', required=True)
+    departure_date = fields.Date(string='Date de départ')
+    destination = fields.Char(string='Destination')
+    partner_id = fields.Many2one(
+        'res.partner',
+        string='Contact',
+        ondelete='cascade'
+    )
